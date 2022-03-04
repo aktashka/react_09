1335f7cdafd36b465228e7c7a34f86dc36bb06ca
aktashka <aktan.imanaliev003@gmail.com>
2022-03-04 15:14:27 +0600

    йоу

---
+++ src/components/app/app.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+import './app.css';
+
+
+class App extends React.Component {
+
+  state = {
+    todos: [
+      { id: 1, label: 'Drink Coffee', important: false, done: false },
+      { id: 2, label: 'Drink tea', important: false, done: false },
+      { id: 3, label: 'Drink vodka', important: false, done: false },
+    ]
+  }
+
+  onDelete = (id) => {
+    console.log(id)
+
+    this.setState((oldState) => {
+      const idx = oldState.todos.findIndex(todo => todo.id === id);
+
+      const prev = oldState.todos.slice(0, idx)
+      const next = oldState.todos.slice(idx + 1)
+
+      const newTodos = [...prev, ...next]
+      
+      return {todos: newTodos}
+    })
+  }
+  onImportant = (id) => {
+    console.log(id)
+
+    this.setState((oldState) => {
+      const idx = oldState.todos.findIndex(todo => todo.id === id);
+
+      const prev = oldState.todos.slice(0, idx)
+      const oldTodo = oldState.todos[idx]
+      const newTodo = {...oldTodo, important: !oldTodo.important}
+      const next = oldState.todos.slice(idx + 1)
+
+      const newTodos = [...prev, newTodo, ...next]
+      
+      return {todos: newTodos}
+    })
+  }
+
+
+  render(){
+    return (
+      <div className="todo-app">
+
+        <AppHeader toDo={1} done={3} />
+
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList onDelete={this.onDelete} onImportant={this.onImportant} todos={this.state.todos} />
+
+      </div>
+    );
+  }
+};
+
+export default App;
---
+++ src/components/todo-list/todo-list.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import TodoListItem from '../todo-list-item';
+import './todo-list.css';
+
+
+const TodoList = ({ todos, onDelete, onImportant }) => {
+
+  // Array generation
+  const content = todos.map((todo) => {
+    // todo -> id, label, important, done
+
+    return (
+      <li key={todo.id} className="list-group-item">
+        <TodoListItem onDelete={onDelete} onImportant={onImportant} {...todo} />
+      </li>
+    );
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      { content }
+    </ul>
+  );
+};
+
+
+export default TodoList;
